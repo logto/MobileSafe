dfb73d012ea34ba66ed3af7957f4128901fefca2
logto <mypasttimes@163.com>
2016-12-15 18:52:34 +0800

    setting

---
+++ src/com/logto/mobilesafe/activity/SettingActivity.java
@@ -0,0 +1,19 @@
+package com.logto.mobilesafe.activity;
+
+import com.logto.mobilesafe.R;
+import com.logto.mobilesafe.R.layout;
+import com.logto.mobilesafe.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SettingActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_setting);
+	}
+
+}
