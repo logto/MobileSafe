63b4ee92f9447500fd4a9ff8180fd0aa37c52163
logto <mypasttimes@163.com>
2016-12-28 19:40:26 +0800

    mobile_guid one--four

--- src/com/logto/mobilesafe/activity/MobileSecurityGuideOneActivity.java
+++ src/com/logto/mobilesafe/activity/MobileSecurityGuideOneActivity.java
@@ -1,26 +1,41 @@
 package com.logto.mobilesafe.activity;
 
-import com.logto.mobilesafe.R;
-import com.logto.mobilesafe.R.layout;
-import com.logto.mobilesafe.R.menu;
-
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.logto.mobilesafe.R;
 
 public class MobileSecurityGuideOneActivity extends Activity {
-
+	private Button bt_next;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mobile_security_guide_one);
+		initView();
+		setListeners();
+	}
+	
+	private void setListeners() {
+		bt_next.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MobileSecurityGuideOneActivity.this, MobileSecurityGuideTwoActivity.class);
+				startActivity(intent);
+				//当前页面关闭
+				finish();
+			}
+		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.mobile_security_guide_one, menu);
-		return true;
+	private void initView() {
+		bt_next = (Button) findViewById(R.id.bt_next);
 	}
+
+
 
 }
---
+++ src/com/logto/mobilesafe/activity/MobileSecurityGuideThreeActivity.java
@@ -0,0 +1,26 @@
+package com.logto.mobilesafe.activity;
+
+import com.logto.mobilesafe.R;
+import com.logto.mobilesafe.R.layout;
+import com.logto.mobilesafe.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MobileSecurityGuideThreeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_mobile_security_guide_three);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.mobile_security_guide_three, menu);
+		return true;
+	}
+
+}
